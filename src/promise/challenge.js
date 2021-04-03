876aa760478c5a40ba99885eb7465869e1932945
Lordcreos <leonardosanchez4h@hotmail.com>
2021-04-02 19:49:17 -0500

    finalizar curso

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -6,7 +6,7 @@ const API ='https://rickandmortyapi.com/api/character/';
 fetchData(API)
 .then(data => {
     console.log(data.info.count);
-    return fetchData(`${API}${data.result[1].id}`)
+    return fetchData(`${API}${data.result[0].id}`)
 })
 
 .then(data=>{
